7a7fec2f0a050f55527915015c12ca9c0dd8be72
Gauthier Le Tat <gauthier.letat@gmail.com>
2025-03-20 15:19:12 +0100

    Renaming

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ road_segments_fiware = get_road_segments_from_json(input_file_json)
 if __name__ == "__main__":
 
     # create_road_segments_and_sensors(input_file_json, 200)
-    # ditto_things = transform_jsonld_to_ditto(input_file_json)
+    ditto_things = transform_jsonld_to_ditto(input_file_json)
 
     # put_policy("my.namespace:RoadSegment")
     # for thing in ditto_things:
