41636d9451a4b20a5c0e35f8eac1c92f3e959d02
Gauthier Le Tat <gauthier.letat@gmail.com>
2025-11-18 15:49:11 +0100

    + correction histogramme_plot.py

--- twins_to_compare/scripts_for_measures/histogramme_plot.py
+++ twins_to_compare/scripts_for_measures/histogramme_plot.py
@@ -9,7 +9,7 @@ csv_filepaths_orion = sorted(glob.glob("violin_plots_data/orion_ld/results/*.csv
 csv_filepaths_scorpio = sorted(glob.glob("violin_plots_data/scorpio/results/*.csv"))
 all_files = csv_filepaths_scorpio + csv_filepaths_orion + csv_filepaths_ditto
 duration = "18000"
-lambdas = "20-40-80"
+lambdas = "5-10-20"
 toremove = []
 for file in all_files:
     if "lambdas_list" in file:
@@ -45,14 +45,13 @@ for f in files_to_plot:
     colonne_delais = []
     deleted = 0
     for i in range(len(colonne_delais_brute)):
-        if 0 < float(colonne_delais_brute[i]) < 1:
+        if 0 < float(colonne_delais_brute[i]) < 10:
             colonne_delais.append(colonne_delais_brute[i])
         else:
             deleted += 1
-    print(deleted)
     df = pd.DataFrame({
-        "delai": colonne_delais_brute,
-        "source": (["f"] * len(colonne_delais_brute))
+        "delai": colonne_delais,
+        "source": (["f"] * len(colonne_delais))
     })
     df["delai"] = (
         df["delai"]
@@ -63,9 +62,10 @@ for f in files_to_plot:
         .str.replace("\"", "")
     )
     df["delai"] = pd.to_numeric(df["delai"], errors="coerce")
+    nombre_nan = df["delai"].isna().sum()
+    print(f"Nombre de NaN : {nombre_nan}")
     df["delai"] = df["delai"] * 1000
     dfs[f] = df
-    print(df["delai"].dtype)
     print(df["delai"].describe())
 
 # --- Création des histogrammes ---
@@ -74,17 +74,19 @@ fig, axes = plt.subplots(1, n, figsize=(6 * n, 6), sharey=True)
 if n == 1:
     axes = [axes]
 colors = {
-    "orion_ld": "#0000ff",  # Bleu
-    "ditto": "#ff0000",     # Rouge
-    "scorpio": "#00ff00"    # Vert
+    "orion_ld": "blue",  # Bleu
+    "ditto": "red",     # Rouge
+    "scorpio": "green"    # Vert
 }
 for ax, f in zip(axes, dfs):
     if "orion" in f:
         title = "Fiware orion_ld"
         source_name = "orion_ld"
+        bins = 20
     elif "ditto" in f:
         title = "Eclipse ditto"
         source_name = "ditto"
+        bins = 50
     elif "scorpio" in f:
         title = "Fiware scorpio"
         source_name = "scorpio"
@@ -94,15 +96,23 @@ for ax, f in zip(axes, dfs):
         hue="source",
         legend=False,
         kde=False,
-        bins=30,  # Vous pouvez ajuster le nombre de bins selon vos préférences
-        color=colors[source_name],
+        bins=bins,  # Vous pouvez ajuster le nombre de bins selon vos préférences
+        palette=[colors[source_name]],
         ax=ax
     )
     ax.set_title(title)
     ax.grid(axis="y", linestyle="--", alpha=0.4)
     ax.set_yscale('log')  # Définir l'échelle logarithmique sur l'axe des y
-    ax.set_xlabel("Délai (ms)")
-    ax.set_ylabel("Fréquence (échelle logarithmique)")
-    ax.set_xticklabels("")
+    ax.set_xlabel("Delay (ms)")
+    ax.set_ylabel("Frequency")
+    # Définir les limites de l'axe x pour s'assurer que toutes les valeurs sont visibles
+    # Ajouter une marge de 10% à gauche et à droite
+    x_min = max(0, dfs[f]["delai"].min() - 0.1 * dfs[f]["delai"].min())
+    x_max = dfs[f]["delai"].max() * 1.1
+    # Si la valeur minimale est 0, définir une marge fixe à gauche
+    if x_min == 0:
+        x_min = -0.1 * dfs[f]["delai"].max()
+    ax.set_xlim(x_min, 70)
+
 plt.tight_layout()
 plt.savefig(f"histogram_plots_figs/duration_{duration}s_lambdas{lambdas}")
