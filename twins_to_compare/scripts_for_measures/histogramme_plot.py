62e0f5974795dd55a7faa78808b79546b3f01a5b
Gauthier Le Tat <gauthier.letat@gmail.com>
2025-11-18 15:04:33 +0100

    + correction nom plot + ajout histogramme_plot.py

---
+++ twins_to_compare/scripts_for_measures/histogramme_plot.py
@@ -0,0 +1,108 @@
+import csv
+import glob
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+
+csv_filepaths_ditto = sorted(glob.glob("violin_plots_data/ditto/results/*.csv"))
+csv_filepaths_orion = sorted(glob.glob("violin_plots_data/orion_ld/results/*.csv"))
+csv_filepaths_scorpio = sorted(glob.glob("violin_plots_data/scorpio/results/*.csv"))
+all_files = csv_filepaths_scorpio + csv_filepaths_orion + csv_filepaths_ditto
+duration = "18000"
+lambdas = "20-40-80"
+toremove = []
+for file in all_files:
+    if "lambdas_list" in file:
+        toremove.append(file)
+for file in toremove:
+    if file in all_files:
+        all_files.remove(file)
+files_to_plot = []
+for f in all_files:
+    if duration in f and lambdas in f:
+        files_to_plot.append(f)
+
+def extraire_colonnes_csv(filepath):
+    with open(filepath, newline='', encoding='utf-8') as f:
+        lecteur = csv.reader(f)
+        entetes = next(lecteur)
+        colonnes = [[] for _ in entetes]
+        for ligne in lecteur:
+            for i, valeur in enumerate(ligne):
+                colonnes[i].append(valeur)
+    return dict(zip(entetes, colonnes))
+
+colonnes = {}
+all_values = []
+all_labels = []
+dfs = {}
+for f in files_to_plot:
+    delais = extraire_colonnes_csv(f)["delay (s)"]
+    for i in range(len(delais)):
+        delais[i] = float(delais[i])
+    colonnes[f] = extraire_colonnes_csv(f)["delay (s)"]
+    colonne_delais_brute = extraire_colonnes_csv(f)["delay (s)"]
+    colonne_delais = []
+    deleted = 0
+    for i in range(len(colonne_delais_brute)):
+        if 0 < float(colonne_delais_brute[i]) < 1:
+            colonne_delais.append(colonne_delais_brute[i])
+        else:
+            deleted += 1
+    print(deleted)
+    df = pd.DataFrame({
+        "delai": colonne_delais_brute,
+        "source": (["f"] * len(colonne_delais_brute))
+    })
+    df["delai"] = (
+        df["delai"]
+        .astype(str)
+        .str.strip()
+        .str.replace(",", ".")
+        .str.replace("'", "")
+        .str.replace("\"", "")
+    )
+    df["delai"] = pd.to_numeric(df["delai"], errors="coerce")
+    df["delai"] = df["delai"] * 1000
+    dfs[f] = df
+    print(df["delai"].dtype)
+    print(df["delai"].describe())
+
+# --- Création des histogrammes ---
+n = len(dfs)
+fig, axes = plt.subplots(1, n, figsize=(6 * n, 6), sharey=True)
+if n == 1:
+    axes = [axes]
+colors = {
+    "orion_ld": "#0000ff",  # Bleu
+    "ditto": "#ff0000",     # Rouge
+    "scorpio": "#00ff00"    # Vert
+}
+for ax, f in zip(axes, dfs):
+    if "orion" in f:
+        title = "Fiware orion_ld"
+        source_name = "orion_ld"
+    elif "ditto" in f:
+        title = "Eclipse ditto"
+        source_name = "ditto"
+    elif "scorpio" in f:
+        title = "Fiware scorpio"
+        source_name = "scorpio"
+    sns.histplot(
+        data=dfs[f],
+        x="delai",
+        hue="source",
+        legend=False,
+        kde=False,
+        bins=30,  # Vous pouvez ajuster le nombre de bins selon vos préférences
+        color=colors[source_name],
+        ax=ax
+    )
+    ax.set_title(title)
+    ax.grid(axis="y", linestyle="--", alpha=0.4)
+    ax.set_yscale('log')  # Définir l'échelle logarithmique sur l'axe des y
+    ax.set_xlabel("Délai (ms)")
+    ax.set_ylabel("Fréquence (échelle logarithmique)")
+    ax.set_xticklabels("")
+plt.tight_layout()
+plt.savefig(f"histogram_plots_figs/duration_{duration}s_lambdas{lambdas}")
